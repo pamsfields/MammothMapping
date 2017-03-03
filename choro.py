dbd159d545910474c7030cc496a3296b6e0ab236
clara <clara@clara.com>
2017-03-03 09:46:56 -0600

    choropleth zoom

--- choro.py
+++ choro.py
@@ -1,4 +1,3 @@
-
 import pandas, folium, json
 
 data = pandas.read_csv('mammoth_data.csv')  # dataframe from CSV
@@ -19,9 +18,8 @@ data['quantity'] = quantities
 state_data_groups = data.groupby(by=['state']).sum().reset_index()
 
 # Add missing states to dataframe.  About 15 states have no mammoths.
-# Choropleth will error if states are missing
-# Extra regions, countries, or states will be ignored
-# Need to add a zero value for missing states.
+# Choropleth will error if states are missing (Extra regions, countries, or states will be ignored)
+# Need to add a row with a zero value for missing states.
 states_abbr = json.load(open('us_states_abbr.json'))
 state_list = list(states_abbr.keys())
 
@@ -38,22 +36,26 @@ all_states_data = all_states_data.drop_duplicates('state', keep='first')
 # Neat! Replace with dictionary of { orig: replace } ; which are in the form { 'Minnesota' : 'MN'}
 state_data_groups = all_states_data.replace(states_abbr)
 
+print(state_data_groups)
+
+
 # And create the choropleth map
 
 us_states_file = r'us_states.json'   # Path to geo-json file
 
-choromap = folium.Map(location=[40, -100], zoom_start=4)
+choromap = folium.Map(location=[40, -120], zoom_start=3)
 
 choromap.choropleth(
     geo_path=us_states_file,   # Reads this file
     data=state_data_groups,
     columns=['state', 'quantity'],
-    key_on='id',     # The key in the geo_path
+    key_on='id',     # The key in the geo_path data
     fill_color='BuPu', fill_opacity=0.6, line_opacity=0.4,
-    threshold_scale=[0,5,10,15,20,90],  # six items max!
+    threshold_scale=[0,5,10,15,20,90],  # six items max
     legend_name="Mammoth finds per state"
 )
 
 choromap.save('mammoth_choropleth.html')
 
-#TODO - display more data when states are hovered over. This would help http://leafletjs.com/examples/choropleth/
+#TODO - display more data when states are hovered over.
+#This would help http://leafletjs.com/examples/choropleth/
